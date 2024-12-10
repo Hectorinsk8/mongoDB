ab3918f43552667ea684672fde42bafc4575139f
Hectorinsk8 <h870392@gmail.com>
2024-12-10 10:54:42 -0600

    Update Index.js

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-const express = require('express');
+import express from 'express';
+import morgan from 'morgan';
+import connect from './utils/db.js';
+import colors from 'colors';
+
+
+//require('dotenv').config();
 
 const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(morgan('combined'))
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended: true}))
+
+app.use((err, req, res, next) => {
+    console.log(err.stack)
+    res.status(500).send('Something broke :('.red.bold)
+})
 
 app.get('/', (req, res) => {
     res.send('Server OK')
 })
 
-app.listen(3000, () => {
-    console.log('Server conected on port 3000')
+app.get('/api', (req, res) => {
+    res.send({
+        message: 'Sorry bro ',
+        author: 'Peruvian',
+        lastname: 'Peru',
+        age: 30,
+
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server conected on: ',`http://localhost:${port}`.blue.bold )
 });
+
+
+connect();
